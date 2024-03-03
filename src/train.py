e60b18dc3c61d2b0cc881914330d119b99ddf0b4
Samsoum41 <hocine.samy.sh@gmail.com>
2024-03-03 01:53:18 +0100

    FQI with paper parameters, 10 iterations

--- src/train.py
+++ src/train.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import os
+from joblib import dump, load
 
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
@@ -13,151 +14,32 @@ env = TimeLimit(
 # Now is the floor is yours to implement the agent and train it.
 
 
-class ReplayBuffer:
-    def __init__(self, capacity, device):
-        self.capacity = capacity # capacity of the buffer
-        self.data = []
-        self.index = 0 # index of the next cell to be filled
-        self.device = device
-    def append(self, s, a, r, s_, d):
-        if len(self.data) < self.capacity:
-            self.data.append(None)
-        self.data[self.index] = (s, a, r, s_, d)
-        self.index = (self.index + 1) % self.capacity
-    def sample(self, batch_size):
-        batch = random.sample(self.data, batch_size)
-        return list(map(lambda x:torch.Tensor(np.array(x)).to(self.device), list(zip(*batch))))
-    def __len__(self):
-        return len(self.data)
-
-class DQN(nn.Module):
-
-    def __init__(self, n_observations, n_actions, nb_neurons = 128):
-        super(DQN, self).__init__()
-        self.layer1 = nn.Linear(n_observations, nb_neurons)
-        self.layer2 = nn.Linear(nb_neurons, nb_neurons)
-        self.layer3 = nn.Linear(nb_neurons, n_actions)
-
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        return self.layer3(x)
-
-def greedy_action(network, state):
-    device = "cuda" if next(network.parameters()).is_cuda else "cpu"
-    with torch.no_grad():
-        Q = network(torch.Tensor(state).unsqueeze(0).to(device))
-        return torch.argmax(Q).item()
+def greedy_action(Q,s,nb_actions):
+    Qsa = []
+    for a in range(nb_actions):
+        sa = np.append(s,a).reshape(1, -1)
+        Qsa.append(Q.predict(sa))
+    return np.argmax(Qsa)
 
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 class ProjectAgent:
     def __init__(self):
-        config = {
-            'learning_rate': 0.001,
-            'gamma': 0.98,
-            'buffer_size': 1000000,
-            'epsilon_min': 0.01,
-            'epsilon_max': 1.,
-            'epsilon_decay_period': 1000,
-            'epsilon_delay_decay': 20,
-            'batch_size': 20}
-        self.gamma = config['gamma']
-        self.batch_size = config['batch_size']
-        self.nb_actions = 4
-        self.nb_observations = 6
-        self.nb_neurons = 128
-        self.model = DQN(self.nb_observations, self.nb_actions,self.nb_neurons)
-        device = "cuda" if next(self.model.parameters()).is_cuda else "cpu"
-        self.memory = ReplayBuffer(config['buffer_size'], device)
-        self.epsilon_max = config['epsilon_max']
-        self.epsilon_min = config['epsilon_min']
-        self.epsilon_stop = config['epsilon_decay_period']
-        self.epsilon_delay = config['epsilon_delay_decay']
-        self.epsilon_step = (self.epsilon_max-self.epsilon_min)/self.epsilon_stop
-        self.criterion = torch.nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=config['learning_rate'])
-
-    def gradient_step(self):
-        if len(self.memory) > self.batch_size:
-            X, A, R, Y, D = self.memory.sample(self.batch_size)
-            QYmax = self.model(Y).max(1)[0].detach()
-            #update = torch.addcmul(R, self.gamma, 1-D, QYmax)
-            update = torch.addcmul(R, 1-D, QYmax, value=self.gamma)
-            QXA = self.model(X).gather(1, A.to(torch.long).unsqueeze(1))
-            loss = self.criterion(QXA, update.unsqueeze(1))
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-
-    def train(self, env, max_episode_length):
-        episode_return = []
-        episode = 0
-        episode_cum_reward = 0
-        state, _ = env.reset()
-        epsilon = self.epsilon_max
-        step = 0
-        max_episode = 200
-        current_episode_length = 0
-        while episode < max_episode:
-            current_episode_length += 1
-            # update epsilon
-            if step > self.epsilon_delay:
-                epsilon = max(self.epsilon_min, epsilon-self.epsilon_step)
-
-            # select epsilon-greedy action
-            if np.random.rand() < epsilon:
-                action = env.action_space.sample()
-            else:
-                action = greedy_action(self.model, state)
-
-            # step
-            next_state, reward, done, trunc, _ = env.step(action)
-            self.memory.append(state, action, reward, next_state, done)
-            episode_cum_reward += reward
-
-            # train
-            self.gradient_step()
-
-            # next transition
-            step += 1
-            if current_episode_length >= max_episode_length:
-                episode += 1
-                print("Episode ", '{:3d}'.format(episode),
-                      ", episode_length ", current_episode_length,
-                      ", epsilon ", '{:6.2f}'.format(epsilon),
-                      ", batch size ", '{:5d}'.format(len(self.memory)),
-                      ", episode return ", '{:4.1f}'.format(episode_cum_reward),
-                      sep='') 
-                state, _ = env.reset()
-                episode_return.append(episode_cum_reward)
-                episode_cum_reward = 0
-                current_episode_length = 0
-            else:
-                state = next_state
-
-        return episode_return
+        self.Q = None
+        self.load()
+        
     def act(self, observation, use_random=False):
         if use_random:
             return env.action_space.sample()
 
-        return greedy_action(self.model, observation)
+        return greedy_action(self.Q, observation, env.action_space.n)
 
     def save(self, path):
         # model_path = "dqn_maxEpLen200_200ep_24neurons"
-        save_path = os.path.join('./', f'{path}.pt')
-        torch.save({
-            # 'episodes_return': returns,
-            'model_state_dict': self.model.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict()
-        }, save_path)
-
+        save_path = os.path.join('./', f'../model/{path}.joblib')
+        dump(self.Q, save_path)
     def load(self):
-        model_path = "dqn_maxEpLen500_200ep_128neurons"
-        save_path = os.path.join('./', f'{model_path}.pt')
-        checkpoint = torch.load(save_path, map_location=torch.device('cpu'))
-        self.model.load_state_dict(checkpoint['model_state_dict'])
-        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        model_name = "Q_value_function_FQI_paper"
+        save_path = os.path.join('./', f'../model/{model_name}.joblib')
+        self.Q = load(save_path)
